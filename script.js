8dab46b7fe8f18269de05a729e22dc86d848ad96
rlr79 <66887463+rlr79@users.noreply.github.com>
2021-04-25 19:27:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -65,3 +65,30 @@ $(function () {
   200);
 
 });
+
+
+
+
+/*
+a Pen by DIACO : twitter.com/Diaco_ml  ||  codepen.io/MAW
+powered by GSAP : https://www.greensock.com/
+*/
+
+
+var total = 30;
+var warp = document.getElementById("fallingRoses"),	w = window.innerWidth , h = "550vw";
+ 
+ for (i=0; i<total; i++){ 
+   var Div = document.createElement('div');
+   TweenLite.set(Div,{attr:{class:'dot'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+   warp.appendChild(Div);
+   animm(Div);
+ }
+ 
+ function animm(elm){   
+   TweenMax.to(elm,R(6,15),{y:h+100,ease:Linear.easeNone,repeat:-1,delay:-15});
+   TweenMax.to(elm,R(4,8),{x:'+=100',rotationZ:R(0,180),repeat:-1,yoyo:true,ease:Sine.easeInOut});
+   TweenMax.to(elm,R(2,8),{rotationX:R(0,360),rotationY:R(0,360),repeat:-1,yoyo:true,ease:Sine.easeInOut,delay:-5});
+ };
+
+function R(min,max) {return min+Math.random()*(max-min)};
